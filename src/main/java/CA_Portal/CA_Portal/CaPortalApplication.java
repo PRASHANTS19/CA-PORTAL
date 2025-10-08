65cdf38361e383c71d7eaff6dcfaab4d7136f63d
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2025-10-08 21:10:39 +0530

    Remove application.properties from tracking

---
+++ src/main/java/CA_Portal/CA_Portal/CaPortalApplication.java
@@ -0,0 +1,13 @@
+package CA_Portal.CA_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CA_Portal/CA_Portal/CaPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package CA_Portal.CA_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
